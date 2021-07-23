8429a5b8b442ba383086f7d321fed0553f742f17
Umar <umermehar1@gmail.com>
2021-07-23 12:33:14 +0930

    added views and url for helloWorld API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+    def get(self, request, format=None):
+        """Returns a list of API View features"""
+        an_apiview = [
+        'Uses HTTP methods as get post put delete',
+        'Is similiar to traditional Django views',
+        'gives you more control over logic',
+        'is mapped manually to URLs',
+        ]
+        return Response({
+        'message':'hello',
+        'an_apiview':an_apiview
+        })
